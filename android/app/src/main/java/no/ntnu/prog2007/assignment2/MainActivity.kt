0d31517166b71e12ef5e0568430238b52593a316
Jonas Johan Solsvik <jonasjso@protonmail.com>
2021-10-21 14:35:05 +0200

    notifyDataSetChanged()

--- android/app/src/main/java/no/ntnu/prog2007/assignment2/MainActivity.kt
+++ android/app/src/main/java/no/ntnu/prog2007/assignment2/MainActivity.kt
@@ -2,21 +2,53 @@ package no.ntnu.prog2007.assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextUtils.isEmpty
+import android.util.Log
+import android.util.Log.INFO
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var groceryList = arrayListOf("hei", "på", "deg")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var groceryList = ArrayList<String>();
-
         // 1. Setup adapter
-        val groceryListView = findViewById<RecyclerView>(R.id.GroceryList)
-        groceryListView.adapter = GroceryListAdapter(groceryList);
-        groceryListView.layoutManager = LinearLayoutManager(this)
+        val adapter =  GroceryListAdapter(groceryList)
+        GroceryList.adapter = adapter
+        GroceryList.layoutManager = LinearLayoutManager(this)
 
-        // 2. Setup 3 buttons onclick-listeners
+
+        // 2. Add listener
+        Add.setOnClickListener {
+            when {
+                AddAnItem.text.isEmpty() -> {println("Error adding item to list: Text is empty")};// @TODO You have to type something in this textbox first
+                else -> {
+                    Log.println(Log.INFO, "MainActivity", "Adding item to list: ${AddAnItem.text}")
+                    groceryList.add(AddAnItem.text.toString())
+                    adapter.notifyDataSetChanged()
+                    AddAnItem.text.clear()
+                }
+            }
+        }
+
+        // 3. Delete listener
+        Delete.setOnClickListener {
+            when (groceryList.size) {
+                0 -> { } // @TODO There are no items left in your grocery list
+                else -> {
+                    Log.println(Log.INFO, "MainActivity", "Removing the last item in the list")
+                    groceryList.removeAt(groceryList.size-1)
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        }
     }
+
 }

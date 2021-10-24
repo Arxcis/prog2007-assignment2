2fe383c4d593585b306984f91e047a42161ecf7e
Jonas Johan Solsvik <jonasjso@protonmail.com>
2021-10-24 14:39:26 +0200

    navigate to recipiesActivity

--- task2-gordon-ramsay/app/src/main/java/no/ntnu/prog2007/assignment2/task2_gordonramsey/RecipesActivity.kt
+++ task2-gordon-ramsay/app/src/main/java/no/ntnu/prog2007/assignment2/task2_gordonramsey/RecipesActivity.kt
@@ -3,7 +3,7 @@ package no.ntnu.prog2007.assignment2.task2_gordonramsey
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Recipes : AppCompatActivity() {
+class RecipesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipes)
